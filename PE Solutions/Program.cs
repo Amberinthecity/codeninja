fdd2da249c56e3e08c499d314b8af1c842267931
Amberinthecity <emailambernow@gmail.com>
2014-02-24 22:11:45 -0500

    PE Problem 1 Solution
    PE Problem 1 Solution using loop in C#

---
+++ PE Solutions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int total = 0;
+            for (int i = 0; i < 1000; i++ )
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine(total);
+
+        }
+
+    }
+}
